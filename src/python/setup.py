00b6fd6e5e10bb61e263d2affc417dfaec0bc42c
saahaand <94850529+saahaand@users.noreply.github.com>
2024-02-11 14:33:52 +0100

    Update setup.py

--- src/python/setup.py
+++ src/python/setup.py
@@ -1,4 +1,52 @@
 from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+from setuptools.dist import Distribution
+from setuptools.command.install import install
+
+
+class BinaryDistribution(Distribution):
+
+    def is_pure(self):
+        return False
+
+    def has_ext_modules(self):
+        return True
+
+
+class InstallPlatlib(install):
+
+    def finalize_options(self):
+        install.finalize_options(self)
+        self.install_lib = self.install_platlib
+
+
+setup(
+    name='cspy',
+    version='1.0.0',
+    author='Sahand',
+    author_email='your.email@example.com',
+    url='https://github.com/saahaand/cspy',
+    description='Modified version of cspy by Mat',
+    distclass=BinaryDistribution,
+    cmdclass={'install': InstallPlatlib},
+    packages=find_packages(),
+    package_data={
+        'cspy': ['.libs/*', 'algorithms/*'],
+    },
+    include_package_data=True,
+    install_requires=['networkx', 'numpy'],
+    classifiers=[
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: C++',
+    ],
+)
 
 setup(
     name='cspy',
